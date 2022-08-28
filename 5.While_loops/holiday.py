05738d4cf427985738fd178f8e886a9de95f5471
ivoivanov0830006 <108527062+ivoivanov0830006@users.noreply.github.com>
2022-08-28 14:17:01 +0300

    Create holiday.py

---
+++ 5.While_loops/holiday.py
@@ -0,0 +1,57 @@
+needed_money = float(input())
+current_money = float(input())
+
+days = 0
+spend_in_row = 0
+
+while current_money < needed_money:
+    text = input()
+    money = float(input())
+    days += 1
+    last_action = text
+    if text == "spend":
+        current_money -= money
+        if last_action == "spend":
+            spend_in_row += 1
+            if spend_in_row == 5:
+                break
+        if current_money < 0:
+            current_money = 0
+    elif text == "save":
+        current_money += money
+if current_money >= needed_money:
+    print(f"You saved the money for {days} days.")
+if spend_in_row == 5:
+    print("You can't save the money.")
+    print(f"{days}")
+
+
+# -----------------ANOTHER_SOLUTION--------------------
+
+needed_money = float(input())
+current_money = float(input())
+
+days = 0
+spend_in_row = 0
+
+while current_money < needed_money:
+    text = input()
+    money = float(input())
+    days += 1
+
+    if text == "spend":
+        current_money -= money
+        spend_in_row += 1
+        if spend_in_row == 5:
+            break
+        if current_money < 0:
+            current_money = 0
+    elif text == "save":
+        current_money += money
+        spend_in_row = 0
+
+if current_money >= needed_money:
+    print(f"You saved the money for {days} days.")
+if spend_in_row == 5:
+    print("You can't save the money.")
+    print(f"{days}")
