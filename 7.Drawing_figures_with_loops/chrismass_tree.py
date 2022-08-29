2d86c40eb32965c34eaea3460bca6d936e8afc8f
ivoivanov0830006 <108527062+ivoivanov0830006@users.noreply.github.com>
2022-08-29 21:48:55 +0300

    Create chrismass_tree.py

---
+++ 7.Drawing_figures_with_loops/chrismass_tree.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(n + 1):
+    stars = "*" * i
+    spaces = " " * (n - i)
+    print(spaces, end="")
+    print(stars, end="")
+    print(" | ", end="")
+    print(stars, end="")
+    print(spaces)
