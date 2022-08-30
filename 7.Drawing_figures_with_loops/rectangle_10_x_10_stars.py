3e0770457527c90283cf3c6fd42dcc4a69b6520e
ivoivanov0830006 <108527062+ivoivanov0830006@users.noreply.github.com>
2022-08-30 21:12:56 +0300

    Create rectangle_10_x_10_stars.py

---
+++ 7.Drawing_figures_with_loops/rectangle_10_x_10_stars.py
@@ -0,0 +1,11 @@
+for rows in range(1, 11):
+    for cols in range(1,11):
+        print("*", end="")
+    print()
+
+# -----------ANOTHER SOLUTION---------------
+#
+# for i in range(1, 11):
+#   print("*" * 10)
+
+#Напишете програма, която чертае на конзолата правоъгълник от 10 x 10 звездички.
