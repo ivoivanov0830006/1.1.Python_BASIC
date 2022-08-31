4331b6df4ac8c64287b9e6eceb481c9bccb63418
ivoivanov0830006 <108527062+ivoivanov0830006@users.noreply.github.com>
2022-08-31 22:23:08 +0300

    Create sunglasses.py

---
+++ 7.Drawing_figures_with_loops/sunglasses.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+print("*" * (2 * n), end="")
+print(" " * n, end="")
+print("*" * (2 * n))
+
+for i in range(n - 2):
+    print("*", end="")
+    print("/" * (2 * n - 2), end="")
+    print("*", end="")
+    if i == (n - 1) // 2 - 1:
+        print("|" * n, end="")
+    else:
+        print(" " * n, end="")
+    print("*", end="")
+    print("/" * (2 * n - 2), end="")
+    print("*")
+
+print("*" * (2 * n), end="")
+print(" " * n, end="")
+print("*" * (2 * n), end="")
