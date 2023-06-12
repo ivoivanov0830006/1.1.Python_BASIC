4dac732954eccf0ea0b51daf26307236053c4165
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-12 14:29:42 +0300

    Create 05.Greeting.py

---
+++ 1.First_steps_in_coding/05.Greeting.py
@@ -0,0 +1,8 @@
+#name = input()
+#print("Hello, " + name + "!")
+# F STRING FORMATIRANE !  !! ! ! !
+name = input()
+print('Hello, ', end='Pesho e pich, ')
+print(name)
+print("alabala")
+#end="" zamenq noviq red, v sluchaq ENTER ( nov red ) ili   \n
