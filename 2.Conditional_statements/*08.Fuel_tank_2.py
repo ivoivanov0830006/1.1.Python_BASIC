2e5c9e70970db17264068ad0586406bf9e95e765
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-20 22:11:46 +0300

    Update *08.Fuel_tank_2.py

--- 2.Conditional_statements/*08.Fuel_tank_2.py
+++ 2.Conditional_statements/*08.Fuel_tank_2.py
@@ -1,23 +1,23 @@
-#fuel_type = input()
-#fuel_tank = float(input())
-#if fuel_type == "Diesel":
-#    if fuel_tank >= 25:
-#        print(f"You have enough diesel.")
-#    else:
-#        print(f"Fill your tank with diesel!")
-#elif fuel_type == "Gasoline":
-#    if fuel_tank >= 25:
-#        print(f"You have enough gasoline.")
-#    else:
-#        print(f"Fill your tank with gasoline!")
-#elif fuel_type == "Gas":
-#    if fuel_tank >= 25:
-#        print(f"You have enough gas.")
-#    else:
-#        print(f"Fill your tank with gas!")
-#else:
-#    print("Invalid fuel!")
-#
+fuel_type = input()
+fuel_tank = float(input())
+if fuel_type == "Diesel":
+   if fuel_tank >= 25:
+       print(f"You have enough diesel.")
+   else:
+       print(f"Fill your tank with diesel!")
+elif fuel_type == "Gasoline":
+   if fuel_tank >= 25:
+       print(f"You have enough gasoline.")
+   else:
+       print(f"Fill your tank with gasoline!")
+elif fuel_type == "Gas":
+   if fuel_tank >= 25:
+       print(f"You have enough gas.")
+   else:
+       print(f"Fill your tank with gas!")
+else:
+   print("Invalid fuel!")
+
 #
 # SECOND SOLUTION
 # fuel_type = (input())
