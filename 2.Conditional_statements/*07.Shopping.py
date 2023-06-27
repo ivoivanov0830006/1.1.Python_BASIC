52ab766089f7351d5db7d0c1b817c36381eab0fd
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-27 22:18:27 +0300

    Create *07.Shopping.py

---
+++ 2.Conditional_statements/*07.Shopping.py
@@ -0,0 +1,28 @@
+budget = float(input())
+video_count = int(input())
+processor_count = int(input())
+memory_count = int(input())
+
+video_price = 250
+total_video = video_count * video_price
+processor_price = 0.35 * total_video
+total_processor = processor_count * processor_price
+memory_price = 0.1 * total_video
+total_memory = memory_count * memory_price
+spent = total_video + total_processor + total_memory
+diff = 0
+if video_count > processor_count:
+    spent = spent - (spent * 0.15)
+    if budget >= spent:
+        diff = abs(budget - spent)
+        print(f"You have {diff:.2f} leva left!")
+    else:
+        diff = abs(budget - spent)
+        print(f"Not enough money! You need {diff:.2f} leva more!")
+else:
+    if budget >= spent:
+        diff = abs(budget - spent)
+        print(f"You have {diff:.2f} leva left!")
+    else:
+        diff = abs(budget - spent)
+        print(f"Not enough money! You need {diff:.2f} leva more!")
