d42e3c6fa45a301d465b18c162373fc3b2f991f7
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-27 22:18:53 +0300

    Update *07.Shopping.py

--- 2.Conditional_statements/*07.Shopping.py
+++ 2.Conditional_statements/*07.Shopping.py
@@ -26,3 +26,28 @@ else:
     else:
         diff = abs(budget - spent)
         print(f"Not enough money! You need {diff:.2f} leva more!")
+        
+        
+# ------------------------------ Another Solution ----------------------------
+#
+# budget = float(input())
+# video_count = int(input())
+# processor_count = int(input())
+# memory_count = int(input())
+#
+# video_price = 250
+# total_video = video_count * video_price
+# processor_price = 0.35 * total_video
+# total_processor = processor_count * processor_price
+# memory_price = 0.1 * total_video
+# total_memory = memory_count * memory_price
+# spent = total_video + total_processor + total_memory
+#
+# if video_count > processor_count:
+#     spent = spent - (spent * 0.15)
+#
+# diff = abs(budget - spent)
+# if budget >= spent:
+#     print(f"You have {diff:.2f} leva left!")
+# else:
+#     print(f"Not enough money! You need {diff:.2f} leva more!")
