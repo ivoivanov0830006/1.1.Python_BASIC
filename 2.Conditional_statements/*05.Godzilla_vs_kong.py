5836af38593e5959b184dc94fb89f003db51b2ad
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-27 22:17:20 +0300

    Update *05.Godzilla_vs_kong.py

--- 2.Conditional_statements/*05.Godzilla_vs_kong.py
+++ 2.Conditional_statements/*05.Godzilla_vs_kong.py
@@ -1,59 +1,47 @@
-holiday = float(input())
-count_puzzle = int(input())
-count_doll = int(input())
-count_bear = int(input())
-count_minion = int(input())
-count_truck = int(input())
-price_puzzle = 2.6
-price_doll = 3
-price_bear = 4.1
-price_minion = 8.2
-price_truck = 2
+budget = float(input())
+stat = int(input())
+clothes = float(input())
+total_clothes = stat * clothes
+decor = 0.1 * budget
 rest = 0
 needed = 0
-total = 0
-total_earn = price_puzzle * count_puzzle + price_doll * count_doll + \
-             price_bear * count_bear + price_minion * count_minion + \
-             price_truck * count_truck
-total_count = count_puzzle + count_doll + count_bear + count_minion + count_truck
-if total_count >= 50:
-    total = total_earn - (0.25 * total_earn)
-    rent = 0.1 * total
-    total = total - rent
-    if total >= holiday:
-        rest = total - holiday
-        print(f"Yes! {rest:.2f} lv left.")
-    else:
-        needed = holiday - total
-        print(f"Not enough money! {needed:.2f} lv needed.")
-elif total_count < 50:
-    total = total_earn
-    rent = 0.1 * total
-    total = total_earn - rent
-    if total >= holiday:
-        rest = total - holiday
-        print(f"Yes! {rest:.2f} lv left.")
-    else:
-        needed = holiday - total
-        print(f"Not enough money! {needed:.2f} lv needed.")
+if stat >= 150:
+    total_clothes = total_clothes - 0.1 * total_clothes
+    expense = decor + total_clothes
+    if budget < expense:
+        needed = expense - budget
+        print(f"Not enough money!")
+        print(f"Wingard needs {needed:.2f} leva more.")
+    elif budget >= expense:
+        rest = budget - expense
+        print(f"Action!")
+        print(f"Wingard starts filming with {rest:.2f} leva left.")
+elif stat < 150:
+    expense = decor + total_clothes
+    if budget < expense:
+        needed = expense - budget
+        print(f"Not enough money!")
+        print(f"Wingard needs {needed:.2f} leva more.")
+    elif budget >= expense:
+        rest = budget - expense
+        print(f"Action!")
+        print(f"Wingard starts filming with {rest:.2f} leva left.")
 
 
 # ------------------------------ Another Solution ----------------------------
 #
-# trip_price = float(input())
-# puzzle_count = int(input())
-# dolls_count = int(input())
-# teddy_count = int(input())
-# minions_count = int(input())
-# trucks_count = int(input())
-#
-# total_sum = puzzle_count * 2.6 + dolls_count * 3 + teddy_count * 4.1 + minions_count * 8.2 + trucks_count * 2
-# all_count = puzzle_count + dolls_count + teddy_count + minions_count + trucks_count
-# if all_count >= 50:
-#     total_sum = total_sum * 0.75
-# total_sum = total_sum - (total_sum * 0.1)
-# difference = abs(total_sum - trip_price)
-# if total_sum >= trip_price:
-#     print(f"Yes! {difference:.2f} lv left.")
+# budget = float(input())
+# count_stat = int(input())
+# price_cloth = float(input())
+# decor = budget * 0.1
+# all_clothes_price = count_stat * price_cloth
+# if count_stat > 150:
+#     all_clothes_price = all_clothes_price * 0.9
+# expenses = decor + all_clothes_price
+# diff = abs(expenses - budget)
+# if expenses <= budget:
+#     print("Action!")
+#     print(f"Wingard starts filming with {diff:.2f} leva left.")
 # else:
-#     print(f"Not enough money! {difference:.2f} lv needed")`
+#     print("Not enough money!")
+#     print(f"Wingard needs {diff:.2f} leva more.")
