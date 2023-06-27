93392177c8ef0d83241cbefbca2e197387c57f7d
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-27 22:14:54 +0300

    Update *04.Toy_store.py

--- 2.Conditional_statements/*04.Toy_store.py
+++ 2.Conditional_statements/*04.Toy_store.py
@@ -36,3 +36,24 @@ elif total_count < 50:
     else:
         needed = holiday - total
         print(f"Not enough money! {needed:.2f} lv needed.")
+
+      
+# --------------------------- Another Solution ----------------------------
+#
+# trip_price = float(input())
+# puzzle_count = int(input())
+# dolls_count = int(input())
+# teddy_count = int(input())
+# minions_count = int(input())
+# trucks_count = int(input())
+#
+# total_sum = puzzle_count * 2.6 + dolls_count * 3 + teddy_count * 4.1 + minions_count * 8.2 + trucks_count * 2
+# all_count = puzzle_count + dolls_count + teddy_count + minions_count + trucks_count
+# if all_count >= 50:
+#     total_sum = total_sum * 0.75
+# total_sum = total_sum - (total_sum * 0.1)
+# difference = abs(total_sum - trip_price)
+# if total_sum >= trip_price:
+#     print(f"Yes! {difference:.2f} lv left.")
+# else:
+#     print(f"Not enough money! {difference:.2f} lv needed")
