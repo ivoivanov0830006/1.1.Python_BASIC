a254bf57878335feabe57c7f8d5b0a533fe485e3
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-07-12 23:58:19 +0300

    Update fruit_shop.py

--- 3.Nested_conditional_statements/fruit_shop.py
+++ 3.Nested_conditional_statements/fruit_shop.py
@@ -45,26 +45,98 @@ if day_is_valid and fruit_is_valid:
     print(f"{final_price:.2f}")
 else:
     print("error")
+
+
+# fruit = input()
+# day = input()
+# quantity = float(input())
+#
+# price = 0
+# total = 0
+#
+# if day == "Saturday" or day == "Sunday":
+#     if fruit == "banana":
+#         price = 2.7
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "apple":
+#         price = 1.25
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "orange":
+#         price = 0.9
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "grapefruit":
+#         price = 1.6
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "kiwi":
+#         price = 3
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "pineapple":
+#         price = 5.6
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "grapes":
+#         price = 4.2
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     else:
+#         print("error")
+# elif day == "Monday" or day == "Tuesday" or day == "Wednesday" or day == "Thursday" or day == "Friday":
+#     if fruit == "banana":
+#         price = 2.5
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "apple":
+#         price = 1.2
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "orange":
+#         price = 0.85
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "grapefruit":
+#         price = 1.45
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "kiwi":
+#         price = 2.7
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "pineapple":
+#         price = 5.5
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     elif fruit == "grapes":
+#         price = 3.85
+#         total = price * quantity
+#         print(f"{total:.2f}")
+#     else:
+#         print("error")
+# else:
+#     print("error")
     
-    
-#Магазин за плодове през работните дни работи на следните цени:
+# Магазин за плодове през работните дни работи на следните цени:
 # плод	banana	apple	  orange	 grapefruit	 kiwi	  pineapple	  grapes
 # цена	2.50	  1.20	  0.85	   1.45	       2.70	  5.50	    3.85
-#През събота и неделя магазинът работи на по-високи цени:
+# През събота и неделя магазинът работи на по-високи цени:
 # плод	banana	apple	  orange	 grapefruit	 kiwi	  pineapple	  grapes
 # цена	2.70	  1.25	  0.90	   1.60	       3.00	  5.60	    4.20
-#Напишете програма, която чете от конзолата следните три променливи, въведени от потребителя 
-#и пресмята цената според цените от таблиците по-горе:
-#⦁	плод  - banana / apple / orange / grapefruit / kiwi / pineapple / grapes;
-#⦁	ден от седмицата  - Monday / Tuesday / Wednesday / Thursday / Friday / Saturday / Sunday;
-#⦁	количество (реално число).
-#Резултатът да се отпечата закръглен с 2 цифри след десетичната точка. 
-#При невалиден ден от седмицата или невалидно име на плод да се отпечата "error". 
+# Напишете програма, която чете от конзолата следните три променливи, въведени от потребителя 
+# и пресмята цената според цените от таблиците по-горе:
+# ⦁	плод  - banana / apple / orange / grapefruit / kiwi / pineapple / grapes;
+# ⦁	ден от седмицата  - Monday / Tuesday / Wednesday / Thursday / Friday / Saturday / Sunday;
+# ⦁	количество (реално число).
+# Резултатът да се отпечата закръглен с 2 цифри след десетичната точка. 
+# При невалиден ден от седмицата или невалидно име на плод да се отпечата "error". 
 #
-#вход	                изход
-#apple                2.40
-#Tuesday
-#2
+# вход	                изход
+# apple                2.40
+# Tuesday
+# 2
 #--------------------------------
 #orange               2.70
 #Sunday
