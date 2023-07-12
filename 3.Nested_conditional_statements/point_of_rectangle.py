64ab9d94b9752f41344b490d5c2c91d40e12fbf7
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-07-12 23:54:49 +0300

    Update point_of_rectangle.py

--- 3.Nested_conditional_statements/point_of_rectangle.py
+++ 3.Nested_conditional_statements/point_of_rectangle.py
@@ -1,37 +1,17 @@
-x1 = float(input())
-y1 = float(input())
-x2 = float(input())
-y2 = float(input())
-x = float(input())
-y = float(input())
-
-if x == x2 and y1 <= y <= y2:
-    print("Border")
-elif x == x1 and y1 <= y <= y2:
-    print("Border")
-elif y == y2 and x1 <= x <= x2:
-    print("Border")
-elif y == y1 and x1 <= x <= x2:
-    print("Border")
+number = int(input())
+if number == 1:
+    print("Monday")
+elif number == 2:
+    print("Tuesday")
+elif number == 3:
+    print("Wednesday")
+elif number == 4:
+    print("Thursday")
+elif number == 5:
+    print("Friday")
+elif number == 6:
+    print("Saturday")
+elif number == 7:
+    print("Sunday")
 else:
-    print("Inside / Outside")
-
-#Напишете програма, която проверява дали точка {x, y} се намира върху някоя от 
-#страните на правоъгълник {x1, y1} – {x2, y2}. Входните данни се четат от конзолата
-#и се състоят от 6 реда въведени от потребителя: десетичните числа x1, y1, x2, y2,
-#x и y (като се гарантира, че x1 < x2 и y1 < y2). Да се отпечата "Border" 
-#(точката лежи на някоя от страните) или "Inside / Outside" (в противен случай). 
-#вход                изход
-#2                   Inside / Outside
-#-3
-#12
-#3
-#8
-#-1
-#-----------------------------------------
-#2                   Border
-#-3
-#12
-#3
-#12
-#-1
+    print("Error")
