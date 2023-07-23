a59021e95dec6fd28216b0395a295826a1f42e32
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-07-23 22:21:04 +0300

    Update odd_or_even_sum.py

--- 4.For_loops/odd_or_even_sum.py
+++ 4.For_loops/odd_or_even_sum.py
@@ -1,12 +1,15 @@
 n = int(input())
+
 odd_sum = 0
 even_sum = 0
+
 for number in range(1, n + 1):
     current_num = int(input())
     if number % 2 == 0:
         even_sum = even_sum + current_num
     else:
         odd_sum = odd_sum + current_num
+        
 if even_sum == odd_sum:
     print(f"Yes")
     print(f"Sum = {even_sum}")
@@ -14,6 +17,7 @@ else:
     diff = abs(odd_sum - even_sum)
     print(f"No")
     print(f"Diff = {diff}")
+    
     
     
 #Да се напише програма, която чете n-на брой цели числа, 
