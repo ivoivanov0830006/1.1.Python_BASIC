f71e5a483605eb3f1e3ad3be8592e121623fec16
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-07-31 22:08:37 +0300

    Update 05.Character_sequence.py

--- 4.For_loops/05.Character_sequence.py
+++ 4.For_loops/05.Character_sequence.py
@@ -2,9 +2,7 @@ text = input()
 for index in range(0, len(text)):
     print(text[index])
 
-# OR
 
-
-text = input()
-for symbol in text:
-   print(symbol)
+# text = input()
+# for symbol in text:
+#    print(symbol)
