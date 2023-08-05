06451afe8d97555686dc17501d4fbaf8bb7ce9ed
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-08-05 17:15:22 +0300

    Update *03.Holiday.py

--- 5.While_loops/*03.Holiday.py
+++ 5.While_loops/*03.Holiday.py
@@ -25,6 +25,35 @@ if spend_in_row == 5:
     print("You can't save the money.")
     print(f"{days}")
 
+# -----------------ANOTHER_SOLUTION--------------------
+
+# needed_money = float(input())
+# current_money = float(input())
+
+# days = 0
+# spend_in_row = 0
+
+# while current_money < needed_money:
+#     text = input()
+#     money = float(input())
+#     days += 1
+
+#     if text == "spend":
+#         current_money -= money
+#         spend_in_row += 1
+#         if spend_in_row == 5:
+#             break
+#         if current_money < 0:
+#             current_money = 0
+#     elif text == "save":
+#         current_money += money
+#         spend_in_row = 0
+
+# if current_money >= needed_money:
+#     print(f"You saved the money for {days} days.")
+# if spend_in_row == 5:
+#     print("You can't save the money.")
+#     print(f"{days}")
 
 # Джеси е решила да събира пари за екскурзия и иска от вас да ѝ помогнете да разбере дали ще успее да 
 # събере необходимата сума. Тя спестява или харчи част от парите си всеки ден. Ако иска да похарчи 
