1bec7927719fd7f2615358afdbd89f517e4ed03d
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-08-06 23:32:09 +0300

    Create 02.Password.py

---
+++ 5.While_loops/02.Password.py
@@ -0,0 +1,9 @@
+username = input()
+password = input()
+
+data = input()
+
+while data != password:                 # ======= IF data ! = password:
+    data = input()                      # =======     data = input()
+print(f"Welcome {username}!")           # ======= ELSE:
+                                        # =======     print(f"Welcome {username}!")
