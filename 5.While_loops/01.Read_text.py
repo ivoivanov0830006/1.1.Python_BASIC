4c5d121eb26c6d0d01454c74fef2b66821780a56
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-08-06 23:31:33 +0300

    Create 01.Read_text.py

---
+++ 5.While_loops/01.Read_text.py
@@ -0,0 +1,11 @@
+# direction = input()
+# while direction != "Stop":
+#     print(direction)
+#     direction = input()
+
+
+while True:
+    text = input()
+    if text == "Stop":
+        break
+    print(f"{text}")
