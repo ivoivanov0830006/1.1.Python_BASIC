5c2f8195408a4983499510088db5b1c59445d661
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-08-10 23:55:35 +0300

    Update 03.Sum_numbers.py

--- 5.While_loops/03.Sum_numbers.py
+++ 5.While_loops/03.Sum_numbers.py
@@ -7,3 +7,12 @@ while total != total_number:
     if total >= total_number:
         break
 print(f"{total}")
+
+# control_number = int(input())
+# sum_of_numbers = 0
+#
+# while sum_of_numbers < control_number:
+#     current_number = int(input())
+#     sum_of_numbers += current_number
+# print(sum_of_numbers)
+
