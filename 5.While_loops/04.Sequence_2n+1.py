29e37710b76850fe8b265798dc0e054c4018ce86
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-08-10 23:54:41 +0300

    Create 04.Sequence_2n+1.py

---
+++ 5.While_loops/04.Sequence_2n+1.py
@@ -0,0 +1,22 @@
+total_numbers = int(input())
+current_number = 0
+
+while current_number != total_numbers:
+    current_number = current_number * 2 + 1
+    if current_number > total_numbers:
+        break
+    print(current_number)
+
+  
+#total_numbers = int(input())
+#current_number = 0
+#while current_number <= total_numbers:
+#   current_number = current_number * 2 + 1
+#   print(current_number)
+
+
+# number = int(input())
+# current_number = 1
+# while current_number <= number:
+#     print(current_number)
+#     current_nummber = current_number * 2 + 1
