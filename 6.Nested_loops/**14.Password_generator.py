6538d57a06510713dcdc2681aa845bebe2cf7fc5
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-08-11 16:11:47 +0300

    Create **14.Password_generator.py

---
+++ 6.Nested_loops/**14.Password_generator.py
@@ -0,0 +1,10 @@
+n = int(input())
+l = int(input())
+
+for symbol_1 in range(1, n):
+    for symbol_2 in range(1, n):
+        for symbol_3 in range(97, l + 97):
+            for symbol_4 in range(97, l + 97):
+                for symbol_5 in range(1, n + 1):
+                    if symbol_5 > symbol_1 and symbol_5 > symbol_2:
+                        print(f"{symbol_1}{symbol_2}{chr(symbol_3)}{chr(symbol_4)}{symbol_5}", end=" ")
